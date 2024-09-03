7b178fa8359dffc224110876d218e0e22c39283e
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-03 15:32:25 -0300

    Initial project

---
+++ src/main/java/br/com/lemes/VLbank/VLbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.lemes.VLbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VLbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VLbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lemes/VLbank/VLbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lemes.VLbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VLbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
