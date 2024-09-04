073b4407f2329e12534bc222768253949f5e53ff
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-04 01:25:46 -0300

    Mapeando model de Transaction

---
+++ src/main/java/br/com/lemes/VLbank/enums/transactions/ExpenseType.java
@@ -0,0 +1,9 @@
+package br.com.lemes.VLbank.enums.transactions;
+
+public enum ExpenseType {
+    TRANSPORTATION,
+    UBER,
+    FOOD,
+    GASOLINE,
+    OTHER
+}
---
+++ src/main/java/br/com/lemes/VLbank/model/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package br.com.lemes.VLbank.model.transaction;
+
+import br.com.lemes.VLbank.enums.transactions.ExpenseType;
+import br.com.lemes.VLbank.model.account.Account;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "expense_type")
+    private ExpenseType expenseType;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDate transactionDate;
+}
